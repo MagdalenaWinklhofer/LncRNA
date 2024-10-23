074c0715cbff066f23ccabea3f61e66440eda42f
Magdalena Winklhofer <magdalena.winklhofer@ibv.uio.no>
2024-10-23 14:34:26 +0200

    feelnc classifier

--- 10_lncRNA_classification/5_1_1_feelnc_filter.sh
+++ 10_lncRNA_classification/5_1_1_feelnc_filter.sh
@@ -8,21 +8,21 @@
 #SBATCH --cpus-per-task=4
 
 # load modules
-
 module load Anaconda3/2022.10
 source ${EBROOTANACONDA3}/bin/activate
 conda activate /cluster/projects/nn8014k/magdalena/conda_environments/feelnc/
 
 
 # change directory to the program script 
-cd /cluster/projects/nn8014k/magdalena/script_FEELnc/
+cd /cluster/work/users/magdalena/lncrna/5_1_feelnc/5_1_1_feelnc_filter/
 
 # Filter
 FEELnc_filter.pl \
- -p 4 --monoex=1 \
- -i /cluster/work/users/magdalena/lncrna/3_stringtie_transcriptome/cc_transcriptome_merged.gtf \
- -a /cluster/work/users/magdalena/lncrna/genome_index/ccar_annotation.gtf  \
- > /cluster/work/users/magdalena/lncrna/5_1_feelnc/feelnc_filter/feelnc_candidate_lncRNA.gtf
+ -p 4 \
+ -i /cluster/work/users/magdalena/lncrna/3_stringtie_transcriptome/cc_transcriptome_all.gtf \
+ -a /cluster/work/users/magdalena/lncrna/genome_index/carcar_annotation_v5.gtf  \
+ --monoex=1 \
+ > /cluster/work/users/magdalena/lncrna/5_1_feelnc/5_1_1_feelnc_filter/feelnc_candidate_lncRNA_all.gtf
 
 # keep monoexonic transcripts (sense and antisense) 
 
--- 10_lncRNA_classification/5_1_3_feelnc_classifier.sh
+++ 10_lncRNA_classification/5_1_3_feelnc_classifier.sh
@@ -15,8 +15,11 @@ conda activate /cluster/projects/nn8014k/magdalena/conda_environments/feelnc/
 # change directory to the program script 
 cd /cluster/work/users/magdalena/lncrna/5_1_feelnc/
 
+
 # Classifier
 FEELnc_classifier.pl \
- -i /cluster/work/users/magdalena/lncrna/5_1_feelnc/5_1_2_feelnc_codpot/feelnc_codpot_out/feelnc_lncRNA_codpot_shuffle.lncRNA.gtf \
- -a /cluster/work/users/magdalena/lncrna/genome_index/ccar_annotation.gtf >  /cluster/work/users/magdalena/lncrna/5_1_feelnc/5_1_3_feelnc_classifier/lncRNA_classes.txt
+ -i /cluster/projects/nn8014k/magdalena/LncRNA/5_1_feelnc/5_1_2_feelnc_codpot_all/feelnc_candidate_lncRNA_all.gtf.lncRNA.gtf \
+ -a /cluster/work/users/magdalena/lncrna/genome_index/carcar_annotation_v5.gtf >  /cluster/work/users/magdalena/lncrna/5_1_feelnc/5_1_3_feelnc_classifier/lncRNA_classes.txt
 # the -p option is not available in the classifier module 
+
+
